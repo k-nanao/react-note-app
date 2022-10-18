0cd097854984846e4f61a0d99be26fbb332007f3
zyyx-k-nanao <k-nanao@zyyx.jp>
2022-10-18 09:14:10 +0900

    noteの追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import uuid from 'react-uuid';
 
 function App() {
+  //追加されるノートを配列で格納する
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: '新しいノート',
+      content: '新しいノートの内容',
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
   return (
     <div className='App'>
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );
