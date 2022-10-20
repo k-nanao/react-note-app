5e5b8ce9807d50937e4486530a7baf31f9fc3e8c
zyyx-k-nanao <k-nanao@zyyx.jp>
2022-10-20 09:12:09 +0900

    Mainコンポーネントの編集を行うことができる機能

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,41 @@ function App() {
   //アクティブになっているかの状態を管理する
   const [activeNote, setActiveNote] = useState(false);
 
+  //ノートを追加する関数
   const onAddNote = () => {
     const newNote = {
       id: uuid(),
       title: '新しいノート',
-      content: '新しいノートの内容',
+      content: '',
       modDate: Date.now(),
     };
     setNotes([...notes, newNote]);
-    console.log(notes);
+    // console.log(notes);
   };
 
+  //ノートを削除する関数
   const onDeleteNote = (id) => {
     const filterNotes = notes.filter((note) => note.id !== id);
     setNotes(filterNotes);
+  };
+
+  //アクティブになっているノートのオブジェクトを取得する関数。idだけではなくオブジェクト全て取ってこれる
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  //Mainコンポーネントのnoteを編集したらSidebarの配列notesは更新されなければならない
+  const onUpDateNote = (updatedNote) => {
+    //修正された新しいノートの配列を返す
+    const upDatedNotesArray = notes.map((note) => {
+      //もしmap関数で展開したノートとこれから編集するノートのidが一緒なら
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+    setNotes(upDatedNotesArray);
   };
 
   return (
@@ -35,7 +56,7 @@ function App() {
         onDeleteNote={onDeleteNote}
         setActiveNote={setActiveNote}
       />
-      <Main />
+      <Main activeNote={getActiveNote()} onUpDateNote={onUpDateNote} />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import './Main.css';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
-const Main = () => {
+const Main = ({ activeNote, onUpDateNote }) => {
+  if (!activeNote) {
+    return <div className='note-nothing'>ノートが選択されてません</div>;
+  }
+
+  const onEditNote = (key, value) => {
+    onUpDateNote({
+      //以前のアクティブになっていたノートに対して
+      ...activeNote,
+      //動的キーtitleなのかcontentなのか自動で判別してくれる
+      [key]: value,
+      modDate: Date.now(),
+    });
+  };
+
   return (
     <div className='app-main'>
       <div className='app-main-note-edit'>
-        <input type='text' />
-        <textarea id='' placeholder='ノート内容を記入'></textarea>
+        <input
+          id='title'
+          type='text'
+          value={activeNote.title}
+          onChange={(e) => onEditNote('title', e.target.value)}
+        />
+        <textarea
+          id='content'
+          placeholder='ノート内容を記入'
+          value={activeNote.content}
+          onChange={(e) => onEditNote('content', e.target.value)}
+        ></textarea>
       </div>
       <div className='app-main-note-preview'>
-        <h1 className='preview-title'>タイトル</h1>
-        <div className='markdown-preview'>ノート内容</div>
+        <h1 className='preview-title'>{activeNote.title}</h1>
+        <ReactMarkdown className='markdown-preview'>
+          {activeNote.content}
+        </ReactMarkdown>
       </div>
     </div>
   );
